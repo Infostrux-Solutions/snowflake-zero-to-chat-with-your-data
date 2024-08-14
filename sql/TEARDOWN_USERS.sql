8ef40b7558cfd350b53965e53ddac99d0bbbc353
Ivaylo Boiadjiev <ivaylo.work@gmail.com>
2024-08-14 11:50:16 -0400

    Remove user warehouses on user teardown

--- sql/TEARDOWN_USERS.sql
+++ sql/TEARDOWN_USERS.sql
@@ -3,7 +3,7 @@
 -- --------------------- --
 -- TEARDOWN USER OBJECTS --
 -- --------------------- --
-SET participant_count = 1;
+SET participant_count = 2;
 
 USE ROLE ACCOUNTADMIN;
 USE WAREHOUSE LAB_ADMIN_WAREHOUSE;
@@ -13,6 +13,7 @@ DECLARE
     schema_prefix STRING DEFAULT 'CHAT_WITH_YOUR_DATA.WORKSPACE_';
     role_prefix STRING DEFAULT 'LAB_USER_ROLE_';
     user_prefix STRING DEFAULT 'LAB_USER_';
+    warehouse_prefix STRING DEFAULT 'LAB_USER_WAREHOUSE_';
 BEGIN
     FOR i IN 1 TO $participant_count DO
             LET user_schema_name := schema_prefix || i;
@@ -21,6 +22,8 @@ BEGIN
             DROP ROLE IF EXISTS IDENTIFIER(:user_role_name);
             LET user_name := user_prefix || i;
             DROP USER IF EXISTS IDENTIFIER(:user_name);
+            LET user_warehouse_name := warehouse_prefix || i;
+            DROP WAREHOUSE IDENTIFIER(:user_warehouse_name);
         END FOR;
     RETURN 'Done';
 END;
