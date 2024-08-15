230df811eaec7d0cd38dfbedd5a4a70d934b91d5
Ivaylo Boiadjiev <ivaylo.work@gmail.com>
2024-08-15 15:20:01 -0400

    Messy code but the data context seems to be working

--- streamlit/chat_with_history.py
+++ streamlit/chat_with_history.py
@@ -1,9 +1,10 @@
+# Based on https://quickstarts.snowflake.com/guide/asking_questions_to_your_own_documents_with_snowflake_cortex/index.html#4
+
 import streamlit as st # Import python packages
 from snowflake.snowpark.context import get_active_session
 session = get_active_session() # Get the current credentials
 
 import pandas as pd
-import re
 
 pd.set_option("max_colwidth",None)
 
@@ -31,12 +32,8 @@ def main():
 
     # Display chat messages from history on app rerun
     for message in st.session_state.messages:
-        if message["role"] == "system":
-            continue
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
-            if "results" in message:
-                st.dataframe(message["results"])
 
     # Accept user input
     if question := st.chat_input("What do you want to know about your products?"):
@@ -55,21 +52,10 @@ def main():
                 response = complete(question)
                 res_text = response[0].RESPONSE
 
-                # st.markdown(response)
-
-                # res_text = res_text.replace("'", "")
+                res_text = res_text.replace("'", "")
                 message_placeholder.markdown(res_text)
 
-                message = {"role": "assistant", "content": response}
-                # Parse the response for a SQL query and execute if available
-                sql_match = re.search(r"```sql\n(.*)\n```", res_text.replace(";", ""), re.DOTALL)
-                if sql_match:
-                    sql = sql_match.group(1)
-                    conn = st.connection("snowflake")
-                    message["results"] = session.sql(sql)
-                    st.dataframe(message["results"])
-                st.session_state.messages.append(message)
-                st.session_state.messages.append({"role": "assistant", "content": res_text})
+        st.session_state.messages.append({"role": "assistant", "content": res_text})
 
 
 def config_options():
@@ -96,8 +82,7 @@ def init_messages():
 
     # Initialize chat history
     if st.session_state.clear_conversation or "messages" not in st.session_state:
-        # st.session_state.messages = []
-        st.session_state.messages = [{"role": "system", "content": get_system_prompt()}]
+        st.session_state.messages = []
 
 
 # def get_similar_chunks (question):
@@ -207,8 +192,6 @@ def create_prompt (myquestion):
     #        </question>
     #        Answer:
     #        """
-
-    # @TODO: Leverage the <context> reference below
     prompt = f"""
            You are an expert chat assistance that extracs information from the CONTEXT provided
            between <context> and </context> tags.
@@ -242,93 +225,6 @@ def complete(myquestion):
 
     df_response = session.sql(cmd, params=[st.session_state.model_name, prompt]).collect()
     return df_response
-
-SCHEMA_PATH = 'CHAT_WITH_YOUR_DATA.WORKSPACE_0'
-QUALIFIED_TABLE_NAME = f"{SCHEMA_PATH}.FINANCIAL_ENTITY_ANNUAL_TIME_SERIES"
-TABLE_DESCRIPTION = """
-This table has various metrics for financial entities (also referred to as banks) since 1983.
-The user may describe the entities interchangeably as banks, financial institutions, or financial entities.
-"""
-# This query is optional if running Frosty on your own table, especially a wide table.
-# Since this is a deep table, it's useful to tell Frosty what variables are available.
-# Similarly, if you have a table with semi-structured data (like JSON), it could be used to provide hints on available keys.
-# If altering, you may also need to modify the formatting logic in get_table_context() below.
-METADATA_QUERY = f"SELECT VARIABLE_NAME, DEFINITION FROM {SCHEMA_PATH}.FINANCIAL_ENTITY_ATTRIBUTES_LIMITED;"
-
-GEN_SQL = """
-You will be acting as an AI Snowflake SQL Expert named Frosty.
-Your goal is to give correct, executable sql query to users.
-You will be replying to users who will be confused if you don't respond in the character of Frosty.
-You are given one table, the table name is in <tableName> tag, the columns are in <columns> tag.
-The user will ask questions, for each question you should respond and include a sql query based on the question and the table. 
-
-{context}
-
-Here are 6 critical rules for the interaction you must abide:
-<rules>
-1. You MUST MUST wrap the generated sql code within ``` sql code markdown in this format e.g
-```sql
-(select 1) union (select 2)
-```
-2. If I don't tell you to find a limited set of results in the sql query or question, you MUST limit the number of responses to 10.
-3. Text / string where clauses must be fuzzy match e.g ilike %keyword%
-4. Make sure to generate a single snowflake sql code, not multiple. 
-5. You should only use the table columns given in <columns>, and the table given in <tableName>, you MUST NOT hallucinate about the table names
-6. DO NOT put numerical at the very front of sql variable.
-</rules>
-
-Don't forget to use "ilike %keyword%" for fuzzy match queries (especially for variable_name column)
-and wrap the generated sql code with ``` sql code markdown in this format e.g:
-```sql
-(select 1) union (select 2)
-```
-
-For each question from the user, make sure to include a query in your response.
-
-Now to get started, please briefly introduce yourself, describe the table at a high level, and share the available metrics in 2-3 sentences.
-Then provide 3 example questions using bullet points.
-"""
-
-@st.cache_data(show_spinner="Loading Frosty's context...")
-def get_table_context(table_name: str, table_description: str, metadata_query: str = None):
-    table = table_name.split(".")
-    columns = session.sql(f"""
-        SELECT COLUMN_NAME, DATA_TYPE FROM {table[0].upper()}.INFORMATION_SCHEMA.COLUMNS
-        WHERE TABLE_SCHEMA = '{table[1].upper()}' AND TABLE_NAME = '{table[2].upper()}'
-        """).to_pandas()
-    columns = "\n".join(
-        [
-            f"- **{columns['COLUMN_NAME'][i]}**: {columns['DATA_TYPE'][i]}"
-            for i in range(len(columns["COLUMN_NAME"]))
-        ]
-    )
-    context = f"""
-Here is the table name <tableName> {'.'.join(table)} </tableName>
-
-<tableDescription>{table_description}</tableDescription>
-
-Here are the columns of the {'.'.join(table)}
-
-<columns>\n\n{columns}\n\n</columns>
-    """
-    if metadata_query:
-        metadata = session.sql(metadata_query).to_pandas()
-        metadata = "\n".join(
-            [
-                f"- **{metadata['VARIABLE_NAME'][i]}**: {metadata['DEFINITION'][i]}"
-                for i in range(len(metadata["VARIABLE_NAME"]))
-            ]
-        )
-        context = context + f"\n\nAvailable variables by VARIABLE_NAME:\n\n{metadata}"
-    return context
-
-def get_system_prompt():
-    table_context = get_table_context(
-        table_name=QUALIFIED_TABLE_NAME,
-        table_description=TABLE_DESCRIPTION,
-        metadata_query=METADATA_QUERY
-    )
-    return GEN_SQL.format(context=table_context)
 
 if __name__ == "__main__":
     main()
